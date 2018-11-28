e753d4496bc3ebfd47cbd62fcc38332362c6bd4e
JattMones <Downhillsailor@gmail.com>
2018-11-28 14:31:08 -0500

    Adding sourcefile and folder

---
+++ src/fugueGenerator.py
@@ -0,0 +1,3 @@
+#Matt Jones, John Scott, Jon Mendez, Carson Quigly
+#Fugue generator
+#TODO: Make code that creates correct sized random melody, create code that transposes melody, create code that extends melody
