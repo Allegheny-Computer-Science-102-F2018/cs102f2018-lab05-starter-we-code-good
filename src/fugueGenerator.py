ab76c4ce2a143f9c00c239a74626fc3698503fc5
quigley-c <quigleyc@allegheny.edu>
2018-11-28 15:10:15 -0500

    removed unnecessary file

--- src/fugueGenerator.py
+++
@@ -1,3 +0,0 @@
-#Matt Jones, John Scott, Jon Mendez, Carson Quigly
-#Fugue generator
-#TODO: Make code that creates correct sized random melody, create code that transposes melody, create code that extends melody
