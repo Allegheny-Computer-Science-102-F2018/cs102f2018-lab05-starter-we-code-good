e7dd48088c7818eda8aed96b310267aa31c909ad
Jonathan Mendez <mendezj@aldenv149.allegheny.edu>
2018-12-06 18:30:11 -0500

    removes broken midi file and deletes one of the unneeded genMus files and renames the other

--- src/musicGenerator.py
+++ src/musicGenerator.py
@@ -8,6 +8,8 @@ from pyknon.genmidi import Midi
 import pyknon.music
 from pyknon.music import NoteSeq
 import random
+import pygame
+
 
 #Takes semantic analysis input
 #def majorMinor():
@@ -16,27 +18,19 @@ import random
 
 #def genRandom():
 
-def genDuration(length):
-    if(length == "short"):
-        durations = {
-            "2", # half note
-        }
-    elif(length == "medium"):
-        durations = {
-            "4", # quarter note
-            }
-    else:
-        durations = {
-            "8", # eighth note
-        }
+
+durations = {
+    "1"
+    "2"
+}
 
 C_major = {
     'A',
-    'B',
+    #'B',
     'C',
     'D',
     'E',
-    'F',
+    #'F',
     'G'
 }
 
@@ -51,22 +45,11 @@ A_minor = {
 }
 def genKey(sentiment):
     if(sentiment == "happy"):
-        key = C_major
+        return C_major
     else:
-        key = A_minor
-def genMidi():
-    #defaults: 1 track, 60bpm
-    midi = Midi(1, tempo = 120)
-    midi.write("midi/test.mid")
+        return A_minor
 
-    #FluidSynth().play_midi('midi/test.mid')
-
-    #fs = FluidSynth()
-    #fs.midi_to_audio('midi/test.mid', 'mp3/test.mp3')
-
-    #Audio("mp3/test.mp3") this plays the mp3
-
-def randomSeq(n, pitches, durations, rests=False):
+def randomSeq(n, pitches, durations, rests=True):
     # Add a rest to the set of pitches if desired.
     if rests:
         pitches.add('r')
@@ -78,11 +61,19 @@ def randomSeq(n, pitches, durations, rests=False):
         this_seq += pitch[0] + duration[0] + ' '
 
     return NoteSeq(this_seq)
+
 sentiment = input("Was it happy or sad?\n")
 key = genKey(sentiment)
-length = input("Was it short, medium, or long?\n")
-durations = genDuration(length)
+#length = input("Was it short, medium, or long?\n")
+#durations = genDuration(length)
 notes = randomSeq(70, key, durations)
 midi = Midi(1, tempo=120)
 midi.seq_notes(notes, track=0)
-midi.write("midi/test1.mid")
+midi.write("midi/audio.mid")
+
+pygame.init()
+pygame.mixer.music.load("midi/audio.mid")
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy():
+    pygame.time.wait(1000)
